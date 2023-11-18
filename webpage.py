8dd10ff51cb3dea8dd89dddcfa7d08ed4ac4f503
Grace <gracedes@bu.edu>
2023-11-18 15:32:52 -0500

    basic idk

--- webpage.py
+++ webpage.py
@@ -2,16 +2,35 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import urllib.request
+import sys
+import csv
+import codecs
+
 st.title('page title') # TODO
 with open('apikey.txt', 'r') as file:
     apikey = file.read().rstrip()
 
-apilink1 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-apilink2 = "?unitGroup=metric&elements=datetime%2Ctemp%2Cdew%2Chumidity%2Cwindspeed%2Ccloudcover%2Cvisibility%2Csolarenergy&include=days%2Cstats%2Cstatsfcst%2Cremote%2Cobs%2Cfcst&key=" + apikey + "&contentType=csv"
+apilink1 = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
+apilink2 = '?unitGroup=metric&elements=name%2Cdatetime%2Ctemp%2Cdew%2Chumidity%2Cwindspeed%2Ccloudcover%2Cvisibility%2Csolarenergy&include=days%2Cstats%2Cstatsfcst%2Cremote%2Cobs%2Cfcst&key=' + apikey + '&contentType=csv'
 
 loc = st.text_input('Location')
 loc = loc.replace(' ', '%20')
 
 apilinkf = apilink1 + loc + apilink2
 if loc != '':
-    st.write(apilinkf)
+    try:         
+        # Parse the results as CSV
+        csvt = pd.read_csv(urllib.request.urlopen(apilinkf))         
+    except urllib.error.HTTPError  as e:
+        ErrorInfo= e.read().decode() 
+        st.write('Error code: ', e.code, ErrorInfo)
+        sys.exit()
+    except  urllib.error.URLError as e:
+        ErrorInfo= e.read().decode() 
+        st.write('Error code: ', e.code,ErrorInfo)
+        sys.exit()
+
+    st.write('Solar energy production data for ' + '**' + csvt['name'][1]+ '**' + ':')
+    csvt = csvt.loc[:,'datetime':]
+    st.write(csvt)
