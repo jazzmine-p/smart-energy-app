207cb8b0c543414ffefe174c2c2560cb14725ae1
Grace <gracedes@bu.edu>
2023-11-18 14:52:10 -0500

    generate api link

--- webpage.py
+++ webpage.py
@@ -3,7 +3,15 @@ import pandas as pd
 import numpy as np
 
 st.title('page title') # TODO
+with open('apikey.txt', 'r') as file:
+    apikey = file.read().rstrip()
+
+apilink1 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+apilink2 = "?unitGroup=metric&include=days&key=" + apikey + "&contentType=csv"
 
 loc = st.text_input('Location')
-st.write('The current location is', loc)
+loc = loc.replace(' ', '%20')
 
+apilinkf = apilink1 + loc + apilink2
+if loc != '':
+    st.write(apilinkf)
