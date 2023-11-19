19a1a6fee9b47abf2e67af6f1dc3b3e756b9ec5c
Grace <gracedes@bu.edu>
2023-11-18 21:12:50 -0500

    idk

--- SolarWise.py
+++ SolarWise.py
@@ -14,7 +14,12 @@ from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-st.title('SolarWise Energy Forecast') # TODO
+st.set_page_config(page_title='SolarWise', page_icon='Solar Wise Logo.png', layout='wide')
+col1, col2 = st.columns([0.1, 0.9], gap="small")
+col1.image('Solar Wise Logo.png', use_column_width=True)
+col2.title('SolarWise Energy Forecast')
+col2.markdown('Enter your location for a 15 day solar panel production forecast')
+# primary color = #427F13
 
 forecast = []
 
@@ -46,14 +51,9 @@ def gen_data(link):
     return forecast
 
 def graph():
-    st.area_chart(forecast, x='datetime', y='solar output (kw/hr)')
-    st.area_chart(forecast, x='datetime', y='temp')
-    st.area_chart(forecast, x='datetime', y='cloudcover')
-    st.area_chart(forecast, x='datetime', y='visibility')
-    st.area_chart(forecast, x='datetime', y='solarenergy')
+    col2.area_chart(forecast, x='Date', y='Predicted Solar Output (kW/hr)', color=(66, 127, 19, 150))
 
 def predict(vs_test):
-    
     df = pd.read_csv("joined-weather-solar.csv")
     df['Date'] = pd.to_datetime(df['Date'])
     df['year'] = df['Date'].dt.year
@@ -71,39 +71,41 @@ def predict(vs_test):
     y = df['Site Performance Estimate']
 
     # Splitting the dfset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=420)
 
     # Normalizing the df
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    # scaler = MinMaxScaler(feature_range=(0, 1))
+    # X_train = scaler.fit_transform(X_train)
+    #X_test = scaler.transform(X_test)
 
     vs_test_df = vs_test
 
     # Preprocess the data
-    vs_test['datetime'] = pd.to_datetime(vs_test['datetime'])
-    vs_test['year'] = vs_test['datetime'].dt.year
-    vs_test['month'] = vs_test['datetime'].dt.month
-    vs_test['day'] = vs_test['datetime'].dt.day
-    vs_test = vs_test.drop(columns=['datetime', 'name'])
+    vs_test['Date'] = pd.to_datetime(vs_test['datetime'])
+    vs_test['year'] = vs_test['Date'].dt.year
+    vs_test['month'] = vs_test['Date'].dt.month
+    vs_test['day'] = vs_test['Date'].dt.day
+    vs_test = vs_test.drop(columns=['Date', 'name'])
 
     # Reorder column and MinMaxScaler
     vs_test = vs_test[['cloudcover', 'visibility', 'temp', 'dew', 'humidity', 'windspeed',
         'solarenergy', 'year', 'month', 'day']]
-    vs_test = scaler.transform(vs_test)
+    # vs_test = scaler.transform(vs_test)
 
     # Predict using AdaBoosting
-    final_ada = AdaBoostRegressor(base_estimator=None, 
-                                learning_rate=0.5, 
+    forestRegressor = RandomForestRegressor(n_estimators = 100, random_state = 42)
+    forestRegressor.fit(X_train, y_train)
+    final_ada = AdaBoostRegressor(base_estimator=forestRegressor, 
+                                learning_rate=5, 
                                 n_estimators=150,
-                                random_state=42)
+                                random_state=420)
     result = final_ada.fit(X_train, y_train)
 
     # Final prediction
     vs_pred = result.predict(vs_test)
 
-    vs_test_df = vs_test_df.join(pd.DataFrame(data=vs_pred, columns=['solar output (kw/hr)']))
-    st.write(vs_test_df.loc[:, ['datetime', 'temp', 'cloudcover', 'visibility', 'solar output (kw/hr)']], use_container_width=True)
+    vs_test_df = vs_test_df.join(pd.DataFrame(data=vs_pred, columns=['Predicted Solar Output (kW/hr)']))
+    col1.dataframe(vs_test_df.loc[:, ['Date', 'Predicted Solar Output (kW/hr)']], use_container_width=True, hide_index=True)
 
     return vs_test_df
   
@@ -112,5 +114,6 @@ loc = st.text_input('Location')         # get location from user
 if loc != '':
     link = gen_link(loc)
     forecast = gen_data(link)
+    col1, col2 = st.columns([0.3, 0.7], gap="small")
     forecast = predict(forecast)
     graph()
